5c7df9894ef51e74f4098de005803ede7d5612c0
rudyards <rudy@mightycheese.com>
2019-09-04 14:23:15 -0400

    Updated banlist (again)

--- search-engine/lib/ban_list/msem2.rb
+++ search-engine/lib/ban_list/msem2.rb
@@ -1,7 +1,6 @@
 BanList.for_format("msem2") do
   format_start(
     "https://snapdragonfirework.wixsite.com/msem2/bans",
-    "Virida the Forgotten One" => "banned",
     "Mystic Meddling " => "banned",
     "Poisoned Well"     => "banned",
     "Savage Congregation" => "banned",
