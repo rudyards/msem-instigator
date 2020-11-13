c6e5a4ef0ce9d881720d6d57a94c7fa58d03a7e2
rudyards <rudy@mightycheese.com>
2020-11-12 23:46:25 -0800

    Finished #15 and #17

---
+++ search-engine/lib/condition/condition_is_investigateland.rb
@@ -0,0 +1,18 @@
+class ConditionIsInvestigateland < Condition
+    def search(db)
+      names = [
+        "backstage rooms","heist headquarters","hidden hollow","legitimate establishment","seedy bar",
+        "cleared vault","forgotten shrine","grim slums","overgrown refuge","unlit alley"
+      ]
+  
+      names
+        .map{|n| db.cards[n]}
+        .flat_map{|card| card ? card.printings : []}
+        .to_set
+    end
+  
+    def to_s
+      "is:investigateland"
+    end
+  end
+end
---
+++ search-engine/lib/condition/condition_is_monofetch.rb
@@ -0,0 +1,16 @@
+class ConditionIsMonofetch < Condition
+    def search(db)
+      names = [
+        "gleaming river","murky peat","sunwashed spires","tropical grove","vast savannah"
+      ]
+  
+      names
+        .map{|n| db.cards[n]}
+        .flat_map{|card| card ? card.printings : []}
+        .to_set
+    end
+  
+    def to_s
+      "is:monofetch"
+    end
+  end
