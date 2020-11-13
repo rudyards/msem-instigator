41227d88494132337d0809b5ebf5964c2eedd5f0
rudyards <rudy@mightycheese.com>
2020-11-12 23:52:05 -0800

    fixed indentation and end issues

--- search-engine/lib/condition/condition_is_cycleland.rb
+++ search-engine/lib/condition/condition_is_cycleland.rb
@@ -1,18 +1,17 @@
 class ConditionIsCycleland < Condition
-    def search(db)
-      names = [
-        "back alley","expansive sprawl","lakeside market","rugged outcropping","weathered slums",
-        "courtroom halls","empty courtyard","ornate interior","torchlit shipyard","waterway dam"
-      ]
-  
-      names
-        .map{|n| db.cards[n]}
-        .flat_map{|card| card ? card.printings : []}
-        .to_set
-    end
-  
-    def to_s
-      "is:cycleland"
-    end
+  def search(db)
+    names = [
+      "back alley","expansive sprawl","lakeside market","rugged outcropping","weathered slums",
+      "courtroom halls","empty courtyard","ornate interior","torchlit shipyard","waterway dam"
+    ]
+
+    names
+      .map{|n| db.cards[n]}
+      .flat_map{|card| card ? card.printings : []}
+      .to_set
+  end
+
+  def to_s
+    "is:cycleland"
   end
 end
