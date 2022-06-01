23f4c6d0bb6dc9f4f9bab90d6ed891f3549960b2
Rudy Summers <rudy@mightycheese.com>
2022-06-01 07:20:44 -0700

    fixed banlists

--- search-engine/lib/ban_list/msedh.rb
+++ search-engine/lib/ban_list/msedh.rb
@@ -7,12 +7,16 @@ BanList.for_format('msedh') do
     'End of the World' => 'banned',
     'Planar Conflagration' => 'banned',
     'Whiterun Gates' => 'banned',
-    'Poisoned Well' => 'banned',
     'Isles Uncharted' => 'banned',
     'Siesa Tremund' => 'banned',
     'Talent Pool' => 'banned',
     'Heart of Zhedina' => 'banned',
     'Kaur, End of Lies' => 'banned',
     'Planar Reflection' => 'banned',
+    'Precursor Sanctum' => 'banned',
+    "Mosecf, the Sunseeker's Path"  => 'banned',
+    'Sea of Skies Beyond'  => 'banned',
+    'Cradle of Corruption'  => 'banned',
+    'Elrayin, the Manaroot'  => 'banned',
   )
 end
